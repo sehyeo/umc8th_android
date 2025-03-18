3658ff3c1d0fe354260452c73147d31087fb71a1
벨라 <icomksh@gmail.com>
2025-03-18 12:01:15 +0900

    activity_main 화면이 표시되도록 MainActivity 수정

--- app/src/main/java/com/example/umc8th/MainActivity.kt
+++ app/src/main/java/com/example/umc8th/MainActivity.kt
@@ -17,16 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Umc8thTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
