73602e5f6d90f25de30ec22bd64f30b6cc78119b
벨라 <icomksh@gmail.com>
2025-03-22 12:07:41 +0900

    스플래시 화면 구현

--- app/src/main/java/com/example/umc8th/SplashActivity.kt
+++ app/src/main/java/com/example/umc8th/SplashActivity.kt
@@ -10,10 +10,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_splash)
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000) // 2초 후 실행
+        }, 2000) // 2초 후 이동
     }
 }
+
