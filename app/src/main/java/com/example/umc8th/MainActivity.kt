305ba94e036843401bb7ec9e53a41f343131027f
벨라 <icomksh@gmail.com>
2025-03-22 13:08:44 +0900

    Feat: fade 애니메이션 효과 추가

--- app/src/main/java/com/example/umc8th/MainActivity.kt
+++ app/src/main/java/com/example/umc8th/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavOptions
 import com.example.umc8th.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,26 +27,38 @@ class MainActivity : AppCompatActivity() {
 
         // BottomNavigationView 아이템 선택 리스너 설정
         binding.bottomNavi.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    navController.navigate(R.id.navigation_home)
-                    true
+            val navOptions = NavOptions.Builder()
+                .setEnterAnim(R.anim.fade_in)
+                .setExitAnim(R.anim.fade_out)
+                .setPopEnterAnim(R.anim.fade_in)
+                .setPopExitAnim(R.anim.fade_out)
+                .build()
+
+            if (item.itemId != navController.currentDestination?.id) {
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        navController.navigate(R.id.navigation_home, null, navOptions)
+                        true
+                    }
+                    R.id.navigation_social -> {
+                        navController.navigate(R.id.navigation_social, null, navOptions)
+                        true
+                    }
+                    R.id.navigation_record -> {
+                        navController.navigate(R.id.navigation_record, null, navOptions)
+                        true
+                    }
+                    R.id.navigation_mypage -> {
+                        navController.navigate(R.id.navigation_mypage, null, navOptions)
+                        true
+                    }
+                    else -> false
                 }
-                R.id.navigation_social -> {
-                    navController.navigate(R.id.navigation_social)
-                    true
-                }
-                R.id.navigation_record -> {
-                    navController.navigate(R.id.navigation_record)
-                    true
-                }
-                R.id.navigation_mypage -> {
-                    navController.navigate(R.id.navigation_mypage)
-                    true
-                }
-                else -> false
+            } else {
+                true
             }
         }
+
     }
 
     fun hideBottomNavigation(state:Boolean){
