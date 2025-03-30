252586ad3001aa93f490c4153275ed10910d5287
벨라 <icomksh@gmail.com>
2025-03-30 16:21:57 +0900

    앨범 클릭 시 AlbumFragment로 이동

--- app/src/main/java/com/example/umc8th/HomeFragment.kt
+++ app/src/main/java/com/example/umc8th/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.umc8th.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,5 +20,13 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeAlbum.setOnClickListener {
+            findNavController().navigate(R.id.albumFragment)
+        }
     }
 }
