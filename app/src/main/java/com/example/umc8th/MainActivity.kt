a9813ed406db55a4d19bf2fd82c0074922118e06
벨라 <icomksh@gmail.com>
2025-04-05 23:35:13 +0900

    Thread 활용하여 스톱워치 제작

--- app/src/main/java/com/example/umc8th/MainActivity.kt
+++ app/src/main/java/com/example/umc8th/MainActivity.kt
@@ -2,36 +2,121 @@ package com.example.umc8th
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.ComponentActivity
+import com.example.umc8th.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private var isRunning = false
+    private var time = 0
+    private var thread: Thread? = null
+    private var paused = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val a = A()
-        val b = B()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        a.start()
-        a.join()
-        b.start()
-    }
-
-    class A : Thread(){
-        override fun run() {
-            super.run()
-            for (i in 1..1000){
-                Log.d("test", "first : $i")
+        binding.btnStart.setOnClickListener {
+            if (!isRunning) {
+                startStopwatch()
+            } else if (paused) {
+                resumeStopwatch()
             }
+        }
+
+        binding.btnPause.setOnClickListener {
+            pauseStopwatch()
+        }
+
+        binding.btnClear.setOnClickListener {
+            clearStopwatch()
         }
     }
 
-    class B : Thread(){
-        override fun run() {
-            super.run()
-            for (i in 1000 downTo 1){
-                Log.d("test", "second : $i")
+    private fun setTimerStatus(isRunning : Boolean) {
+
+        if(isRunning){
+            binding.btnStart.visibility = View.GONE
+            binding.btnPause.visibility = View.VISIBLE
+        } else{
+            binding.btnStart.visibility = View.VISIBLE
+            binding.btnPause.visibility = View.GONE
+        }
+    }
+
+    private fun startStopwatch() {
+        isRunning = true
+        paused = false
+        setTimerStatus(true)
+        thread = Thread {
+            while (isRunning) {
+                if (!paused) {
+                    Thread.sleep(100)
+                    time++
+                    runOnUiThread {
+                        val sec = time / 10
+                        val milli = time % 10
+                        binding.timeText.text = String.format("%02d.%d", sec, milli)
+                    }
+                }
             }
         }
+        thread?.start()
+    }
+
+    private fun pauseStopwatch() {
+        paused = true
+        setTimerStatus(false)
+    }
+
+    private fun resumeStopwatch() {
+        paused = false
+        setTimerStatus(true)
+    }
+
+    private fun clearStopwatch() {
+        time = 0
+        runOnUiThread {
+            binding.timeText.text = "00.0"
+        }
+
+        if (paused) {
+            isRunning = false
+            thread = null
+            setTimerStatus(false)
+        }
+
     }
 }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val a = A()
+//        val b = B()
+//
+//        a.start()
+//        a.join()
+//        b.start()
+//    }
+//
+//    class A : Thread(){
+//        override fun run() {
+//            super.run()
+//            for (i in 1..1000){
+//                Log.d("test", "first : $i")
+//            }
+//        }
+//    }
+//
+//    class B : Thread(){
+//        override fun run() {
+//            super.run()
+//            for (i in 1000 downTo 1){
+//                Log.d("test", "second : $i")
+//            }
+//        }
+//    }
