6b0ba98172e739ed218ee34d0ac5ecdbd7d6b311
벨라 <icomksh@gmail.com>
2025-04-05 13:56:30 +0900

    스레드 간단 실습

--- app/src/main/java/com/example/umc8th/MainActivity.kt
+++ app/src/main/java/com/example/umc8th/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.example.umc8th
 
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import android.content.Intent
-import com.example.umc8th.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        val a = A()
+        val b = B()
 
-        // 감정 스탬프 클릭 시 EmotionActivity로 이동 (Intent에 감정 타입 전달)
-        binding.emotionStamp1.setOnClickListener { navigateToEmotionActivity("happy") }
-        binding.emotionStamp2.setOnClickListener { navigateToEmotionActivity("excited") }
-        binding.emotionStamp3.setOnClickListener { navigateToEmotionActivity("normal") }
-        binding.emotionStamp4.setOnClickListener { navigateToEmotionActivity("worried") }
-        binding.emotionStamp5.setOnClickListener { navigateToEmotionActivity("angry") }
+        a.start()
+        a.join()
+        b.start()
     }
 
-    private fun navigateToEmotionActivity(emotion: String) {
-        val intent = Intent(this, EmotionActivity::class.java)
-        intent.putExtra("emotion_type", emotion)
-        startActivity(intent)
+    class A : Thread(){
+        override fun run() {
+            super.run()
+            for (i in 1..1000){
+                Log.d("test", "first : $i")
+            }
+        }
+    }
+
+    class B : Thread(){
+        override fun run() {
+            super.run()
+            for (i in 1000 downTo 1){
+                Log.d("test", "second : $i")
+            }
+        }
     }
 }
