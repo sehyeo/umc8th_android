58df2428c5505d10471d0b81c279c9cc807e4399
벨라 <icomksh@gmail.com>
2025-04-13 16:26:19 +0900

    위젯 변경 및 Dialog 활용

--- app/src/main/java/com/example/umc8th/MemoActivity.kt
+++ app/src/main/java/com/example/umc8th/MemoActivity.kt
@@ -2,11 +2,12 @@ package com.example.umc8th
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.umc8th.databinding.ActivityMemoBinding
 
-class MemoActivity : ComponentActivity() {
+class MemoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMemoBinding
 
     private var savedMemoText: String? = null
@@ -38,6 +39,25 @@ class MemoActivity : ComponentActivity() {
         if(!savedMemoText.isNullOrBlank()){
             binding.editTextMemo.setText(savedMemoText)
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        AlertDialog.Builder(this)
+            .setTitle("확인")
+            .setMessage("다시 작성하시겠습니까?")
+            .setPositiveButton("예") { dialog, _ ->
+                dialog.dismiss()
+                // 아무것도 하지 않음 -> onResume()에서 복원됨
+            }
+            .setNegativeButton("아니요") { dialog, _ ->
+                dialog.dismiss()
+                // 저장된 내용 삭제 및 EditText 초기화
+                savedMemoText = null
+                binding.editTextMemo.setText("")
+            }
+            .show()
     }
 
 }
